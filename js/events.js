c4b0b1ff0b17209dcfa18c444de7d04575559375
Kayla Hansen <shiverr@gmail.com>
2017-09-18 00:58:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,6 +19,14 @@ function pressIt() {
   });
 }
 
+function submitIt() {
+  $('form').on("submit", function() {
+    if ($( "input:first").val === "correct") {
+      alert('your form is going to be submitted now');
+    }
+});
+}
+
 $(document).ready(function(){
 
 // call functions here
