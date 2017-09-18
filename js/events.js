cd919d2cc48eafc0961819881a5a52d77eb7cc54
Kayla Hansen <shiverr@gmail.com>
2017-09-18 00:59:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,8 +20,8 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form').on("submit", function() {
-    if ($( "input:first").val === "correct") {
+  $("form").on("submit", function() {
+    if ($( "input:first" ).val() === "correct") {
       alert('Your form is going to be submitted now.');
       return;
     }
